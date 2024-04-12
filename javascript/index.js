02e2e5038eb652d77f7b4d91e8a943747026ad55
Mayra Alv <158468578+mayra-alvarez@users.noreply.github.com>
2024-04-11 23:12:02 -0400

    Add files via upload
    ejecios que me dejaron para hacer

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+let A = 5;
+let B = 7;
+let resultado = A + B;
+
+console.log(resultado);
+
+let n1 = 100;
+let n2 = 50;
+let salida = n1 - n2;
+
+ console.log(salida);
+
+let a = 5;
+let e = 5;
+let ejecutar = a * e;
+console.log( ejecutar);
+
+
+let c = 14;
+let d = 2;
+let subir = 14/2;
+
+console.log( subir );
+
+let num1 = 9;
+let num2 = 7;
+let ejecucion = num1^num2;
+console.log(ejecucion);
